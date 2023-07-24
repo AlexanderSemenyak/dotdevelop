2bc55aa95d6fc3584ce6021fcf1e3cc1ff6c8cab
Damian Suess <djsuess@gmail.com>
2023-07-24 10:16:05 -0400

    Updated winbuild.bat to include VS2022 for MSBuild path and semi-verbose output

--- main/winbuild.bat
+++ main/winbuild.bat
@@ -1,6 +1,9 @@
 @echo off
 setlocal enableextensions enabledelayedexpansion
 
+REM Ensure we're executing from this directory
+cd /D "%~dp0"
+
 if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" (
 	FOR /F "delims=" %%E in ('"%ProgramFiles(x86)%\Microsoft Visual Studio\installer\vswhere.exe" -latest -property installationPath') DO (
 		set "MSBUILD_EXE=%%E\MSBuild\Current\Bin\MSBuild.exe"
@@ -11,6 +14,8 @@ if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" (
 )
 
 FOR %%E in (Enterprise, Professional, Community) DO (
+	set "MSBUILD_EXE=%ProgramFiles%\Microsoft Visual Studio\2022\%%E\MSBuild\Current\Bin\MSBuild.exe"
+	if exist "!MSBUILD_EXE!" goto :build
 	set "MSBUILD_EXE=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\%%E\MSBuild\Current\Bin\MSBuild.exe"
 	if exist "!MSBUILD_EXE!" goto :build
 	set "MSBUILD_EXE=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\%%E\MSBuild\15.0\Bin\MSBuild.exe"
@@ -37,8 +42,13 @@ exit /b 1
 
 :build
 
+echo Using MSBuild: '%MSBUILD_EXE%'
+echo Syncing Git Submodules...
+
 git submodule sync || goto :error
 git submodule update --init --recursive || goto :error
+
+echo Restoring solution...
 "external\nuget-binary\NuGet.exe" restore Main.sln
 
 "%MSBUILD_EXE%" external\fsharpbinding\.paket\paket.targets /t:RestorePackages /p:PaketReferences="%~dp0external\fsharpbinding\MonoDevelop.FSharpBinding\paket.references"
@@ -46,6 +56,7 @@ git submodule update --init --recursive || goto :error
 if not defined CONFIG (set "CONFIG=DebugWin32")
 set "PLATFORM=Any CPU"
 
+echo Building solution...
 "%MSBUILD_EXE%" Main.sln /bl:MonoDevelop.binlog /r /m "/p:Configuration=%CONFIG%" "/p:Platform=%PLATFORM%" %* || goto :error
 goto :eof
 
